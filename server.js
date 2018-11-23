807651f2557373602302f5fca1e01d193301901a
Coding Monkey <Maxwell.W.Allee@gmail.com>
2018-11-23 17:16:44 -0500

    pushing between comps

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db = require("./models")
 const passport = require("./utils/passport")
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const jwt = require('jsonwebtoken')
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -19,8 +19,31 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post("/login", (req, res)=> {
+  const {email, password } =req.body
+  db.User.findOne({
+    email: req.body.email
+  },(err, user) => {
+    if(err) throw err;
+
+    if (!user) {
+      return res.status(401).json({success: false, msg: "Authentication failed."})
+    }
+    
+    if (password === user.password){
+      const token = jwt.sign(user.toJSONM(), settings.secret);
+        res.json({success: true, token: 'JWT' + token})
+    }
+    else {
+      res.status(401).send({success: false, msg: "Authentication failed. Wrong password"})
+     }
+  })
+})
+
+
+
 app.use(passport.initialize())
-app.get("/api/test", passport.authenticate('jwt'),(req,res)=>{
+app.get("/api/test", passport.authenticate('jwt',{session:false}),( req,res)=>{
   res.send("It's working!!")
 })
 
